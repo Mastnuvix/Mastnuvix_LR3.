bfe88b553d405005c4fe3b6abcf859375cc5b522
Maria Uvaeva <mariauvaeva@gmail.com>
2025-03-31 13:54:14 +0300

    add function  1

--- Uvaeva_LR3 _project.cpp
+++ Uvaeva_LR3 _project.cpp
@@ -2,24 +2,36 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
+float x = 0;
+float y = 0;
+float razn = x - y;
 void raznost(){
 //задание 1 ввести вещественное число х
-          cout << "Результат задания 1:" << endl;
+cout << "ввести вещественное число х" << endl;
+cin >> x;  
+cout << "Результат задания 1:" << endl;
 }
 void razn(){
 //задание 2 ввести вещественное число y
-          cout << "Результат задания 2:" << endl;
+cout << "ввести вещественное число y" << endl;
+cin >> y;  
+cout << "Результат задания 2:" << endl;
 }
 void zad(){
 //задание 3 найти целую часть разности этих 2-х чисел
-          cout << "Результат задания 3:" << endl;
+int trash = static_cast<int>(razn)
+cout << trash << endl;
+cout << "Результат задания 3:" << endl;
 }
 void zadan(){
 //задание 4 найти дробную часть разности этих 2-ч чисел
-          cout << "Результат задания 4:" << endl;
+double rulka = razn - trash;
+cout << rulka << endl;
+cout << "Результат задания 4:" << endl;
 }   
 }
 int main()
 {
+setlocale(LC_ALL,"rus");
 return 0;
 }
