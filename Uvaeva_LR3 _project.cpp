92880ff5e76778a39a13e1692c5857874dc77e47
Maria Uvaeva <mariauvaeva@gmail.com>
2025-03-31 22:54:25 +0300

    add function 1 2.0

--- Uvaeva_LR3 _project.cpp
+++ Uvaeva_LR3 _project.cpp
@@ -1,37 +1,62 @@
+#include <iostream>
+#include <cmath> 
 #include <limits>
-#include <cmath>
-#include <algorithm>
 using namespace std;
-float x = 0;
-float y = 0;
-float razn = x - y;
-void raznost(){
-//задание 1 ввести вещественное число х
-cout << "ввести вещественное число х" << endl;
-cin >> x;  
-cout << "Результат задания 1:" << endl;
+
+double Num(const std::string& pup) {
+    double number;
+    cout << pup;
+    cin >> number;
+    return number;
 }
-void razn(){
-//задание 2 ввести вещественное число y
-cout << "ввести вещественное число y" << endl;
-cin >> y;  
-cout << "Результат задания 2:" << endl;
+
+
+int fruct(double x, double y) {
+    return static_cast<int>(x - y); 
 }
-void zad(){
-//задание 3 найти целую часть разности этих 2-х чисел
-int trash = static_cast<int>(razn)
-cout << trash << endl;
-cout << "Результат задания 3:" << endl;
+
+double vegetable(double x, double y) {
+    double difference = x - y;
+    return difference - static_cast<int>(difference); 
 }
-void zadan(){
-//задание 4 найти дробную часть разности этих 2-ч чисел
-double rulka = razn - trash;
-cout << rulka << endl;
-cout << "Результат задания 4:" << endl;
-}   
-}
-int main()
-{
-setlocale(LC_ALL,"rus");
-return 0;
+
+int main() {
+    double x, y;
+    
+    x = Num("Введите вещественное число x: ");
+    y = Num("Введите вещественное число y: ");
+
+    int choice;
+    
+    cout << "Выберите действие:\n";
+    cout << "1. Найти целую часть разности\n";
+    cout << "2. Найти дробную часть разности\n";
+    cout << "Введите номер действия (1 или 2): ";
+    int keys;
+        cout << "Введите цифру от 1 или 2:";
+        cin >> keys;
+        if (!cin.good()) {
+        
+        cout<<"\nВведите числа!\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+    switch (keys) {
+        case 1: {
+            int fru = fruct(x, y);
+                cout << "Целая часть разности: " << fru << endl;
+            break;
+        }
+        case 2: {
+            double veg = vegetable(x, y);
+                cout << "Дробная часть разности: " << veg << endl;
+            break;
+        }
+        default:
+                cout << "Неверный выбор. Пожалуйста, введите 1 или 2." << endl;
+            break;
+    }
+
+    return 0;
 }
